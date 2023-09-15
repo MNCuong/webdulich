ecf649f94961f12a5e64d189b15711709bf8f802
MNCuong <100787138+MNCuong@users.noreply.github.com>
2023-09-15 16:00:24 +0700

    Update xacnhandatphong.php

--- xacnhandatphong.php
+++ xacnhandatphong.php
@@ -22,11 +22,6 @@
             border-radius: 10px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
             text-align: center;
-            
-        }
-
-        .confirmation-form:hover {
-            background-color: rgba(255, 255, 255, 0.2);
         }
 
         .confirmation-form h1 {
@@ -38,7 +33,6 @@
             display: block;
             font-size: 16px;
             margin-bottom: 10px;
-			
         }
 
         .confirmation-form input[type="text"], input[type="date"] {
@@ -63,18 +57,57 @@
         .confirmation-form button:hover {
             background-color: #e74c3c;
         }
+
+        .confirmation-message {
+            margin-top: 20px;
+            font-size: 16px;
+            color: #fff;
+        }
     </style>
 </head>
 <body>
     <div class="confirmation-form">
         <h1>Xác Nhận Đặt Phòng</h1>
-        <label for="tenkhach">Tên khách hàng:</label>
-        <input type="text" id="tenkhach" name="tenkhach" required>
-        <label for="ngayden">Ngày đến:</label>
-        <input type="date" id="ngayden" name="ngayden" required>
-        <label for="ngaydi">Ngày đi:</label>
-        <input type="date" id="ngaydi" name="ngaydi" required>
-        <button>Xác nhận</button>
+        <form method="post" action="xacnhandatphong_controller.php">
+            <label for="tenkhach">Tên khách hàng:</label>
+            <input type="text" id="tenkhach" name="tenkhach" required>
+            <label for="ngayden">Ngày đến:</label>
+            <input type="date" id="ngayden" name="ngayden" required>
+            <label for="ngaydi">Ngày đi:</label>
+            <input type="date" id="ngaydi" name="ngaydi" required>
+            <button type="submit">Xác nhận</button>
+        </form>
+    </div>
+    <div class="confirmation-message">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $tenkhach = $_POST["tenkhach"];
+            $ngayden = $_POST["ngayden"];
+            $ngaydi = $_POST["ngaydi"];
+
+            $host = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "anh";
+
+            $conn = new mysqli($host, $username, $password, $database);
+
+            if ($conn->connect_error) {
+                die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
+            }
+
+            // Chèn thông tin đặt phòng vào cơ sở dữ liệu
+            $sql = "INSERT INTO datphong (tenkhach, ngayden, ngaydi) VALUES ('$tenkhach', '$ngayden', '$ngaydi')";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo "Đặt phòng thành công!";
+            } else {
+                echo "Có lỗi xảy ra trong quá trình đặt phòng: " . $conn->error;
+            }
+
+            $conn->close();
+        }
+        ?>
     </div>
 </body>
 </html>
